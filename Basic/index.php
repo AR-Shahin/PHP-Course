5ffe78c00deda7b29674f8ed2eb28ae09cecf2c9
AR-Shahin <mdshahinmije96@gmail.com>
2022-07-04 22:47:16 +0600

    operator

---
+++ Basic/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$num1 = 3;
+$num2 = 2;
+
+
+// $num1 = $num1 + 10;
+
+// $num1 += 10;
+// $num1 -= 10;
+// $num1 *= 10;
+
+// echo $num1;
+// echo $num1 ** $num2;
+
+echo -- $num1 . "<br>";
+
+echo "18  number line : " . $num1 -- . "<br>";
+
+echo "20 number line : " . $num1;
+
