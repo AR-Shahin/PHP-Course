21fe79c615e3d766fe14067cb189199ed444e02f
AR-Shahin <mdshahinmije96@gmail.com>
2022-07-05 21:38:10 +0600

    html in php

--- Basic/index.php
+++ Basic/index.php
@@ -1,46 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>HTML in PHP</h1>
+    <?php
+        for ($i=0; $i <5 ; $i++) { 
+            echo "<h5>HTML in PHP</h5>";
+        }
+    ?>
 
-// if(!function_exists("funName")){
-//     function funName(){
-//         echo "Hello World";
-//     }
-// }
-
-function funName($name) : string
-{
-    return "Hello $name <br>";
-}
-
-// echo funName("World");
-// echo funName("Test");
-
-
-function info(string $name,int $age = 15) :void{
-
-    echo "Name : $name <br>";
-    echo "Age : $age <br>";
-}
-
-$name = "Test";
-// info(age: 20,name :$name);
-
-
-function display(...$params) :void{
-    var_dump($params[0]);
-
-}
-
-
-// display(10,20,30,40,50,60,"test");
-
-
-$sum = function($a,$b){
-    return $a + $b;
-};
-
-// echo $sum(100,20);
-
-$sub = fn($a,$b) =>  $a - $b;
-
-
-echo $sub(20,10);
+    <?= "Hello World"?>
+</body>
+</html>
