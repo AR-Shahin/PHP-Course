37d34f510c745e38444f6798771c1839fe2c1ee1
AR-Shahin <mdshahinmije96@gmail.com>
2022-07-05 23:07:35 +0600

    super global

--- Basic/index.php
+++ Basic/index.php
@@ -1,38 +1,16 @@
 <?php
 
-# Built in Function In PHP
+# Super Global Variable In PHP
 
 $a = 10;
-$arr = [10,20,30,"test"];
-// var_dump($arr);
+$b =20;
+$c = 30;
 
-// print_r($arr );
+function test(){
+    // global $a,$b,$c;
+    // echo $a + $b + $c;
 
-$str = "hello world test abs";
+    echo $GLOBALS['a'] + $GLOBALS['b'] + $GLOBALS['c'];
+}
 
-// $test = explode("-",$str);
-// $test = implode("-",$arr);
-// echo ($test);
-
-// if(in_array(10,$arr)){
-//     echo "Exist";
-// }else{
-//     echo "Not Exist";
-// }
-echo "tt";
-// echo count($arr);
-// die();
-// exit();
-$a = "abc";
-
-// echo md5($a);
-
-// echo strlen($a);
-// echo str_word_count($str);
-
-// echo strtolower($a);
-// echo strtoupper($a);
-
-echo ucfirst($str);
-
-// echo ucwords($str);
+test();
