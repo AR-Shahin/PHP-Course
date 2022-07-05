00c070b8340681b710531d2ddae8f4564d6ce075
AR-Shahin <mdshahinmije96@gmail.com>
2022-07-05 22:09:48 +0600

    Array

--- Basic/index.php
+++ Basic/index.php
@@ -1,19 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>HTML in PHP</h1>
-    <?php
-        for ($i=0; $i <5 ; $i++) { 
-            echo "<h5>HTML in PHP</h5>";
-        }
-    ?>
+<?php
 
-    <?= "Hello World"?>
-</body>
-</html>
+# Array 
+
+$arr = array(15,20,30,50,"PHP");
+
+$brr = [10,20,30,40,50];
+
+// for($i=0;$i<count($brr);$i++){
+//     echo "Array value " . $brr[$i] . "<br>";
+// }
+
+// $students = array("name" => "PHP","age" => 22);
+$students = ["name" => "PHP","age" => 22];
+// echo $students["age"];
+
+// foreach($students as  $value){
+//     echo " Value $value <br>";
+// }
+
+
+$c = [[10,20],200,300];
+
+// var_dump($c[1]);
+
+
+$d = [10,20,30];
+
+// array_push($d,40);
+// $e = array_reverse($d);
+$sum = array_sum($d);
+var_dump($sum);
