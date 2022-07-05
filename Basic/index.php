cc7cecaac125f0b31cd9d16cc8ab5370c19e993e
AR-Shahin <mdshahinmije96@gmail.com>
2022-07-05 23:01:34 +0600

    buit in functions

--- Basic/index.php
+++ Basic/index.php
@@ -1,10 +1,38 @@
 <?php
 
-# Date Time In PHP
-date_default_timezone_set("Asia/Dhaka");
-// echo date("Y:m:d H:i:s a");
+# Built in Function In PHP
 
- $today = date("F j, Y, g:i a"); 
+$a = 10;
+$arr = [10,20,30,"test"];
+// var_dump($arr);
 
+// print_r($arr );
 
-echo strtotime($today);
+$str = "hello world test abs";
+
+// $test = explode("-",$str);
+// $test = implode("-",$arr);
+// echo ($test);
+
+// if(in_array(10,$arr)){
+//     echo "Exist";
+// }else{
+//     echo "Not Exist";
+// }
+echo "tt";
+// echo count($arr);
+// die();
+// exit();
+$a = "abc";
+
+// echo md5($a);
+
+// echo strlen($a);
+// echo str_word_count($str);
+
+// echo strtolower($a);
+// echo strtoupper($a);
+
+echo ucfirst($str);
+
+// echo ucwords($str);
