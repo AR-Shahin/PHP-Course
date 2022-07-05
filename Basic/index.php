24de64237aa7104474a22c6bd46dc29eda207902
AR-Shahin <mdshahinmije96@gmail.com>
2022-07-05 22:23:47 +0600

    variable scope

--- Basic/index.php
+++ Basic/index.php
@@ -1,32 +1,19 @@
 <?php
 
-# Array 
+# Variable Scope in PHP
 
-$arr = array(15,20,30,50,"PHP");
+$num = 10;
 
-$brr = [10,20,30,40,50];
+function fun(){
+    // global $num;
+    static $num2 = 100;
+    echo $num2;
+    $num2 += 50;
+}
 
-// for($i=0;$i<count($brr);$i++){
-//     echo "Array value " . $brr[$i] . "<br>";
-// }
-
-// $students = array("name" => "PHP","age" => 22);
-$students = ["name" => "PHP","age" => 22];
-// echo $students["age"];
-
-// foreach($students as  $value){
-//     echo " Value $value <br>";
-// }
-
-
-$c = [[10,20],200,300];
-
-// var_dump($c[1]);
-
-
-$d = [10,20,30];
-
-// array_push($d,40);
-// $e = array_reverse($d);
-$sum = array_sum($d);
-var_dump($sum);
+fun();
+echo "-------";
+fun();
+echo "-------";
+fun();
+// echo $num2;
