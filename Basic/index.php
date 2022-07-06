147760baef89d0f3bb688a4911f7816d907450d2
AR-Shahin <mdshahinmije96@gmail.com>
2022-07-06 22:52:50 +0600

    oop

--- Basic/index.php
+++ Basic/index.php
@@ -1,8 +1,44 @@
 <?php
 
-# Session Global Variable In PHP
+class User {
+    public $name;
+    public $age;
+    public $email;
 
-session_start();
+    public function display(){
+       echo $this->name;
+    }
 
-$_SESSION['name'] = "PHP Learning";
-$_SESSION['another'] = [10,20,30];
+    public function play(){
+        
+    }
+
+    public function eat(){
+        
+    }
+}
+
+class Student{
+    public $name;
+}
+
+$user1 = new User();
+$user1->name = "User 1";
+
+// echo $user1->display();
+
+echo "<br>";
+$user2 = new User();
+// $user2->name = "User 2";
+
+// echo $user2->name;
+
+
+$stu = new Student();
+
+if($stu instanceof Student)
+{
+    echo "Yes";
+}else{
+    echo "NO";
+}
