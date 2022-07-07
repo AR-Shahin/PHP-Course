a31798bccc5da1cb934d49c5a87268bba1fc1841
AR-Shahin <mdshahinmije96@gmail.com>
2022-07-07 12:01:17 +0600

    oop constant

--- OOP/index.php
+++ OOP/index.php
@@ -1,11 +1,13 @@
 <?php
 
-# Magic Method in PHP
+# Constant In Class
 
 class User {
-    // public $name;
-    // public $age;
-    // public $email;
+    public $name;
+    public $age;
+    public $email;
+
+    public const PI = 3.1416;
 
     public static $address;
 
@@ -17,9 +19,9 @@ class User {
         
     }
 
-    public function eat(){
-        
-    }
+   public function area($r){
+        echo self::PI * $r * $r;
+   }
 
     public static function test(){
         echo self::$address;
@@ -29,68 +31,14 @@ class User {
         echo self::$address;
     }
     
-    // public function __construct(string $n,int $a)
-    // {
-    //     $this->name = $n;
-    //     $this->age = $a;
-    // }
-
-    // public function __construct(public $name,public $age, public $email)
-    // {
-        
-    // }
-
-    // public function __destruct()
-    // {
-    //     echo "<br> I am calling from destructor";
-    // }
-
-    // public function __invoke()
-    // {
-    //     echo "I am invoking <br>";
-    // }
-
-    public $data = [];
-    public function __get($property)
-    {
-        if(array_key_exists($property,$this->data)){
-            return $this->data[$property];
-        }else{
-
-            echo "This $property doesn't exits! <br>";
-        }
-    }
-
-    public function __set($key,$val){
-        $this->data[$key] = $val;
-    }
-
-    public function __call($name, $arguments)
-    {
-        var_dump($name);
-        echo "<br>";
-        var_dump($arguments);
-    }
-    public static function __callStatic($name, $arguments)
-    {
-        var_dump($name);
-        echo "<br>";
-        var_dump($arguments);
-    }
+   
 
 }
 
 
-// (new User())();
+
 
  
 $user = new User();
-// $user->anything = "abc";
-// echo $user->anything;
-// echo "<br>";
-// $user->abc = 123;
-// echo $user->abc;
-
-// $user->any(10,20,30);
-
-User::any("test");
+echo $user->area(3);
+// echo User::PI;
