56dc8bc15dbb717368fd33a7892b4e1225ad3de6
AR-Shahin <mdshahinmije96@gmail.com>
2022-07-09 21:58:05 +0600

    include

--- Basic/index.php
+++ Basic/index.php
@@ -1,31 +1,12 @@
 <?php
 
-if($_SERVER['REQUEST_METHOD'] == "GET"){
-    $name = $_GET['name'];
-    $email = $_GET['email'];
-
-    echo "Name : $name <br> Email $email";
-}
+// include_once "./another.php";
 
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="./index.php" method="GET">
-        <input type="text" name="name" placeholder="Enter Name "> <br>
-        <input type="email" name="email" placeholder="Enter email "> <br>
-        <button>Submit</button>
-    </form>
-</body>
-</html>
+require_once "./another.php";
+require_once "./another.php";
+require_once "./another.php";
+require_once "./another.php";
 
 
- 
-
+echo "Index FIle";
